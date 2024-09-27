6f0b37f9b1df4f2a0ba7a739aeb28c744fd89791
Gabriel Castillo Mendieta <gcastillo@serempre.com>
2024-09-26 19:40:39 -0500

    add docker compose and fixes middleware

--- middelware/index.js
+++ middelware/index.js
@@ -4,30 +4,43 @@ const axios = require('axios');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const SERVER1_IP = process.env.SERVER1_IP;
-const SERVER2_IP = process.env.SERVER2_IP;
-const PORT1 = process.env.PORT1;
-const PORT2 = process.env.PORT2;
+const SERVER1_IP = process.env.SERVER1_IP || '127.0.0.1';
+const SERVER2_IP = process.env.SERVER2_IP || '127.0.0.1';
+const PORT1 = process.env.PORT1 || 4000;
+const PORT2 = process.env.PORT2 || 4001;
 const HEALTH_ENDPOINT = '/health';
 
 let server1HealthData = {};
 let server2HealthData = {};
 
+let server1ConnectionLog = [];
+let server2ConnectionLog = [];
+
 async function fetchHealthData() {
   try {
-    const server1Response = await axios.get(`http://${SERVER1_IP}:${PORT1}/health`);
+    const server1Response = await axios.get(`http://${SERVER1_IP}:${PORT1}${HEALTH_ENDPOINT}`);
     server1HealthData = server1Response.data;
-    console.log('Datos de salud de server1:', server1HealthData);
+    console.log('Datos de salud del backend 1:', server1HealthData);
+
+    server1ConnectionLog.push({ timestamp: new Date(), status: 'connected' });
   } catch (error) {
     server1HealthData = { status: 'ERROR', error: error.message };
+    console.error('Error al obtener datos del backend 1:', error.message);
+
+    server1ConnectionLog.push({ timestamp: new Date(), status: 'ERROR' });
   }
 
   try {
-    const server2Response = await axios.get(`http://${SERVER2_IP}:${PORT2}/health`);
+    const server2Response = await axios.get(`http://${SERVER2_IP}:${PORT2}${HEALTH_ENDPOINT}`);
     server2HealthData = server2Response.data;
-    console.log('Datos de salud de server2:', server2HealthData);
+    console.log('Datos de salud del backend 2:', server2HealthData);
+
+    server2ConnectionLog.push({ timestamp: new Date(), status: 'connected' });
   } catch (error) {
     server2HealthData = { status: 'ERROR', error: error.message };
+    console.error('Error al obtener datos del backend 2:', error.message);
+
+    server2ConnectionLog.push({ timestamp: new Date(), status: 'ERROR' });
   }
 
   console.log('Datos de salud actualizados');
@@ -41,9 +54,11 @@ app.get('/data', (req, res) => {
   res.status(200).json({
     server1: server1HealthData,
     server2: server2HealthData,
+    server1History: server1ConnectionLog,
+    server2History: server2ConnectionLog,
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Middleware corriendo en http://localhost:${PORT}`);
 });
